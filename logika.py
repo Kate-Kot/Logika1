a9fb69277420d0a59bfe00097b98ddae7bf06ec8
Kate-Kat <katt_s@mail.ru>
2023-01-24 15:16:05 +0300

    first commit

---
+++ logika.py
@@ -0,0 +1,27 @@
+import random
+
+lista = list(range(1, 11))
+listb = list(range(1, 11))
+def chet():
+    random.shuffle(lista)
+    random.shuffle(listb)
+    print(lista, len(lista))
+    print(listb, len(listb))
+    k = 0
+    while k < 10 :
+        b = listb[k]
+        k +=1
+        for a in lista:
+            ot = input(f'Подскажи, сколько будет {a} * {b} ?')
+            if ot.isdigit():
+                otvet = int(ot)
+            else:
+                otvet = 0
+
+            resh = a*b
+            if otvet == resh:
+                    print('Ура! Правильно! Ты молодец!!!\n')
+            else:
+                    print(f'Мне жаль, но тут ошибка. {a} * {b} = {resh}\n')
+
+chet()
